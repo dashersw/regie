5ba2836198fd2f86896775a9ac69ce5e0f013284
Armagan Amcalar <armagan@amcalar.com>
2019-12-19 00:56:47 +0100

    Add mapState example

--- example.js
+++ example.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-new */
+
 const regie = require('.')
 
 const initialState = {
   navigation: {}
 }
 
-const { state, observe } = regie({ initialState })
+const { state, observe, $$register } = regie({ initialState }, { deep: true })
 
 observe('navigation.status.value', (newValue, change) => {
   console.log('ust', 'nv', newValue, 'c', change.newValue, '====')
@@ -16,3 +18,25 @@ observe('navigation.status', (newValue, change) => {
 
 state.navigation = { status: { value: 3 } }
 state.navigation = { status: { value: 10 } }
+
+class Component {
+  constructor () {
+    this.created()
+  }
+
+  created () {
+    this.createdHooks()
+  }
+
+  ['mapState navigation.status.value'] (newValue, change) {
+    console.log('yay', JSON.stringify(newValue))
+  }
+
+  dispose () { }
+}
+
+$$register({ Component })
+new Component()
+
+state.navigation = { status: { value: 5 } }
+state.navigation.status = { value: 3 }
